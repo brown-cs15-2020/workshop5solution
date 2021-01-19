eabc6161e886de6008052c4168b3082f03c4b3e2
abigail <abigail_marks@brown.edu>
2021-01-19 15:52:19 -0500

    added cloud class

---
+++ Cloud.java
@@ -0,0 +1,36 @@
+package workshop5solution;
+
+import javafx.scene.shape.Circle;
+
+public class Cloud {
+    private int _type;
+    private Bubble[] _cloudBubbles;
+    public Cloud(int type){
+        _cloudBubbles =new Bubble[4];
+        _type=type;
+    }
+    public void generateCloud(){
+        for( int i =0; i<4; i++){
+            _cloudBubbles[i]=new Bubble();
+        }
+    }
+
+    public void arrangeCloud(){
+        int [][] coordinates;
+        if(_type==0){
+            coordinates = Constants.CLOUD_0;
+        }else{
+            coordinates = Constants.CLOUD_1;
+        }
+        for(int i =0; i<_cloudBubbles.length; i++){
+            _cloudBubbles[i].setX(coordinates[i][0]);
+            _cloudBubbles[i].setY(coordinates[i][1]);
+        }
+    }
+
+    public Circle[] getComps(){
+        Circle[] circleArray =  {_cloudBubbles[0]._getBubble(),_cloudBubbles[1]._getBubble(),
+        _cloudBubbles[2]._getBubble(),_cloudBubbles[3]._getBubble()};
+        return circleArray;
+    }
+}
--- Constants.java
+++ Constants.java
@@ -3,4 +3,9 @@ package workshop5solution;
 public class Constants {
     public static final int SCENE_WIDTH = 900;
     public static final int SCENE_HEIGHT = 600;
+
+    public static final int[][] CLOUD_0 = {{0,0},{1,1},{2,1},{3,1}};
+    public static final int[][] CLOUD_1 = {{0,0},{1,1},{2,1},{3,1}};
+
+
 }
