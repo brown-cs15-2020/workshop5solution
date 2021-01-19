55d7875c1395e2583a6897b2c152f92198db1343
amarks3 <71113283+amarks3@users.noreply.github.com>
2021-01-19 13:05:27 -0800

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -4,8 +4,9 @@ public class Constants {
     public static final int SCENE_WIDTH = 900;
     public static final int SCENE_HEIGHT = 600;
 
-    public static final int[][] CLOUD_0 = {{0,0},{1,1},{2,1},{3,1}};
-    public static final int[][] CLOUD_1 = {{0,0},{1,1},{2,1},{3,1}};
+    public static final int[][] CLOUD_0 = {{405,100},{410,100},{415,100},{420,100}};
+    public static final int[][] CLOUD_1 = {{400,100},{405,105},{400,105},{405,100}};
+
 
 
 }
